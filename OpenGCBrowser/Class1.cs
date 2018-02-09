0ca34939a940acbc78f4e9319b4793b2a0265800
pulemjot <anny.vasiljeva@gmail.com>
2018-02-09 23:32:04 +0200

    Add project files.

---
+++ OpenGCBrowser/Class1.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using NUnit.Framework;
+
+namespace OpenGCBrowser
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void OpenGC()
+        {
+            var myDriver = new ChromeDriver(@"C:\ChromeDriver");
+            myDriver.Navigate().GoToUrl("http://stackoverflow.com/");
+        }
+    }
+}
